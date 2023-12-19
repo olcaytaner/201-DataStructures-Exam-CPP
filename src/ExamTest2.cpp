1856addd2c75407396d933e756a6c93ebbbc7b73
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-12-19 13:44:49 +0300

    Initial setup.

---
+++ src/ExamTest2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Olcay Taner YILDIZ on 19.12.2023.
+//
+#include "List/Graph/Graph.h"
+
+using namespace list;
+
+int main(){
+    Graph g = Graph(10);
+    g.addEdge(1, 2);
+    g.addEdge(2, 4);
+}
